b234aaa9b6e3b1b29811e6598017f1ff99772eb5
Renyi03 <yinyeji@gmail.com>
2025-12-13 15:15:08 +0100

    Added map class

---
+++ Source/Map.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Module.h"
+class Application;
+
+class Map {
+public:
+	Map(Application* app, Module* _listener);
+	~Map();
+
+	bool Start();
+	bool Update();
+	bool CleanUp();
+};
